f81f8898426acaf4bc3f67307abf2f56af79d38f
Z User <z@container>
2026-06-06 20:15:38 +0000

    Fix build_openmp.bat: pawncc uses sym=val for defines, not -D

--- build_openmp.bat
+++ build_openmp.bat
@@ -10,8 +10,9 @@ if exist "openmp-server\gamemodes\fg-ogc.amx" del "openmp-server\gamemodes\fg-og
 
 :: Compile with OPENMP_BUILD flag defined
 :: This replaces SKY plugin natives with open.mp compatible stubs
-:: Note: -I (capital I) for include path, -D for define
-pawno\pawncc.exe gamemodes\fg-ogc.pwn -DOPENMP_BUILD -Ipawno\include -oopenmp-server\gamemodes -;+
+:: Note: pawncc uses sym=val syntax for defines (NOT -D like C compilers)
+::       -i for include path, -o for output, -;+ for semicolon required
+pawno\pawncc.exe gamemodes\fg-ogc.pwn OPENMP_BUILD=1 -ipawno\include -oopenmp-server\gamemodes -;+
 
 echo.
 if exist "openmp-server\gamemodes\fg-ogc.amx" (
