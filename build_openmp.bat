d0435dcc7b4be21a6ca6595572f9e02b4012ccba
Z User <z@container>
2026-06-06 20:14:08 +0000

    Fix build_openmp.bat: use -I (capital) for include path, delete old amx before compile, fix false success detection

--- build_openmp.bat
+++ build_openmp.bat
@@ -5,16 +5,20 @@ echo   Compiling fg-ogc.amx for OPEN.MP
 echo ============================================
 echo.
 
+:: Delete old amx first so we know if recompilation succeeded
+if exist "openmp-server\gamemodes\fg-ogc.amx" del "openmp-server\gamemodes\fg-ogc.amx"
+
 :: Compile with OPENMP_BUILD flag defined
 :: This replaces SKY plugin natives with open.mp compatible stubs
-pawno\pawncc.exe gamemodes\fg-ogc.pwn -DOPENMP_BUILD -i pawno\include -o openmp-server\gamemodes -;+
+:: Note: -I (capital I) for include path, -D for define
+pawno\pawncc.exe gamemodes\fg-ogc.pwn -DOPENMP_BUILD -Ipawno\include -oopenmp-server\gamemodes -;+
 
 echo.
 if exist "openmp-server\gamemodes\fg-ogc.amx" (
     echo [OK] fg-ogc.amx compiled for open.mp!
     echo [OK] Saved to: openmp-server\gamemodes\fg-ogc.amx
 ) else (
-    echo [GRESKA] Kompajliranje nije uspjelo!
+    echo [GRESKA] Kompajliranje nije uspjelo! fg-ogc.amx nije kreiran.
 )
 echo.
 pause
